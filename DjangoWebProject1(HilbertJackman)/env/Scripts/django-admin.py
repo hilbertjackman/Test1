b727aec3ba3df7376990d6c991c2b2bb575a052c
Hilbert Jackman <hilbertjackman@gmail.com>
2022-03-08 22:08:49 -0400

    First Commit

---
+++ DjangoWebProject1(HilbertJackman)/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\hilbe\source\repos\DjangoWebProject1(HilbertJackman)\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
